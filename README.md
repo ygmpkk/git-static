8eb86d29097b67f13aeb199896b0c4e210e7dc89
Timothy <ygmpkk@gmail.com>
2018-11-07 09:24:53 +0800

    Add return 404, and configure branch name, add license

---
+++ README.md
@@ -0,0 +1,13 @@
+# Git pages static
+
+A github pages implements for any git repository hosting
+
+## Usage
+
+Production deployment
+
+```
+gunicorn -w 4 -b 127.0.0.1:5000 app:app
+```
+
+# License MIT
--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-# from werkzeug.contrib.fixers import ProxyFix
-
-from flask import Flask, Response
+from flask import Flask, Response, redirect
 from git import Repo
 
 app = Flask('gitstatic')
 app.config.from_pyfile('config.cfg')
 repository_path = app.config.get('REPOSITORY_PATH')
-# app.wsgi_app = ProxyFix(app.wsgi_app)
+default_branch = app.config.get('BRANCH')
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<string:path>")
-@app.route("/<path:path>")
+@app.route('/', defaults={'path': ''})
+@app.route('/<string:path>')
+@app.route('/<path:path>')
 def home(**kw):
     status = 200
     mime_type = 'text/html'
@@ -18,23 +16,30 @@ def home(**kw):
     try:
         path = kw.get('path', '').split('/')
 
+        # e.g:
+        # user/repo/xxx.html
+        # user/repo/resource/index.html
         user = path[0:1][0]
         repo = path[1:2][0]
         index = '/'.join(path[2:])
 
-        if (not (user and repo)):
-            return
+        if len(path) == 2:
+            return redirect(request.host_url + kw.get('path') + '/index.html')
 
-        repo = Repo('{repo_path}/{user}/{repo}'.format(repo_path=repository_path, user=user, repo=repo))
-        branch = repo.heads['gh-pages']
+        if len(path) == 3 and index == '':
+            return redirect(request.host_url + kw.get('path') + 'index.html')
+
+        if (not (user and repo)):
+            return 'Not Found', 404
+
+        repo = Repo('{repo_path}/{user}/{repo}.git'.format(repo_path=repository_path, user=user, repo=repo))
+        branch = repo.heads[default_branch]
         blob = branch.object.tree[index]
 
         resp = Response(blob.data_stream.read(), status)
-        resp.headers['Content-Type'] = '{type}; charset=utf-8'.format(type=blob.mime_type or mime_type)
+        resp.headers['Content-Type'] = blob.mime_type or mime_type
         resp.headers['Content-Length'] = blob.size
 
         return resp
     except:
-        return 'Not Found', 404, { 'Content-Type': mime_type }
-
-
+        return 'Not Found', 404
